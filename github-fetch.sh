026f90510912dbf768bf812cd136a0997f3b5520
Casey Link <unnamedrambler@gmail.com>
2022-02-18 09:34:35 +0100

    Change download path

--- github-fetch.sh
+++ github-fetch.sh
@@ -3,14 +3,14 @@
 set -eux
 
 # download latest repo build
-REPO_FINAL="$(dirname $0)/sway-desktop"
+REPO_FINAL="/srv/repo/sway-desktop"
 REPO="${REPO_FINAL}.new"
 
-CURL="curl -u token:$(cat ~/.config/github-token) --show-error --fail"
+CURL="curl -u token:$(cat ~/.config/github-token) --show-error --fail "
 RESPONSE=$($CURL --silent https://api.github.com/repos/Ramblurr/workstation-ostree-custom/actions/artifacts)
 ZIP=$(echo "$RESPONSE" | jq --raw-output '.artifacts | map(select(.name == "repository"))[0].archive_download_url')
 echo "INFO: Downloading $ZIP ..."
-[ -e /tmp/repository.zip ] || $CURL -L -o /tmp/repository.zip "$ZIP"
+[ -e /tmp/repository.zip ] || $CURL --continue-at - -L -o /tmp/repository.zip "$ZIP"
 rm -rf "$REPO"
 mkdir -p "$REPO"
 unzip -p /tmp/repository.zip | tar -xzC "$REPO"
